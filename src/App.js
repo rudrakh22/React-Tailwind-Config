9f85769eb4d9c79dc768d566aed5b80cc793466d
rudraksh22 <rudrakshg1914@gmail.com>
2024-02-03 22:09:15 +0530

    React Tailwind Config

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="font-bold text-center mt-[40px] w-11/12 mx-auto rounded-md bg-orange-400 text-3xl py-3 text-white">
+        HelloWorld
+    </div>
+  );
+}
+
+export default App;
